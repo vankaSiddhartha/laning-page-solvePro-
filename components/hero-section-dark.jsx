d537784a8d52cf6c209003ad6811706a46a4b9c8
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-02-12 10:17:42 +0530

    added yt link

--- components/hero-section-dark.jsx
+++ components/hero-section-dark.jsx
@@ -177,7 +177,7 @@ const HeroSection = React.forwardRef(({ className, ...props }, ref) => {
               <span className="relative inline-block overflow-hidden rounded-full p-[1.5px]">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <div className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 text-xs font-medium backdrop-blur-3xl">
-                  <a href="https://drive.google.com/file/d/1YpqFnVIUg9l-tgua0HxqB6swySJAiNVs/view" className="inline-flex rounded-full text-center group items-center w-full justify-center bg-gradient-to-tr from-purple-950 via-purple-900 to-transparent text-white border-purple-800/30 border-[1px] hover:bg-gradient-to-tr hover:from-purple-900 hover:via-purple-800 hover:to-transparent transition-all sm:w-auto py-4 px-10">
+                  <a href="https://drive.google.com/file/d/1zHdtxSyMaUb1KSu0nUrfLzV3CF7xJO7l/view?usp=sharing" className="inline-flex rounded-full text-center group items-center w-full justify-center bg-gradient-to-tr from-purple-950 via-purple-900 to-transparent text-white border-purple-800/30 border-[1px] hover:bg-gradient-to-tr hover:from-purple-900 hover:via-purple-800 hover:to-transparent transition-all sm:w-auto py-4 px-10">
                     Download App
                   </a>
                 </div>
